dd11a3a5f839ef8484afb85e6f8e611dab2f4fca
sujin <orego.jin@gmail.com>
2022-11-21 17:02:22 -0500

    added functions to add, delete and edit text

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-// import "firebase/compat/firestore";
+import "firebase/compat/firestore";
 // import "firebase/compat/storage";
 
 // Your web app's Firebase configuration
@@ -19,3 +19,4 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,57 @@
-import React from "react";
+import Nweet from "components/Nweet";
+import { dbService } from "fbase";
+import React, {useEffect, useState} from "react";
 
-const Home = () => {
+const Home = ({userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  // const getNweets = async () => {
+  //   const dbNweets = await dbService.collection("nweets").get();
+  //   dbNweets.forEach((document)=> {
+  //     const nweetObj = {
+  //       ...document.data(),
+  //       id: document.id,
+  //     }
+  //     setNweets((prev) => [nweetObj,...prev])
+  //   })
+  // };
+
+  useEffect(()=> {
+    // getNweets();
+    dbService.collection('nweets').onSnapshot(snapshot => {
+      const nweetArray = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})); //map : does not re-render
+      setNweets(nweetArray);
+    })
+  }, [])
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    dbService.collection('nweets').add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid
+    });
+    setNweet("");
+  }
+
+  const onChange = (e) =>{
+    const {target : {value}} = e;
+    setNweet(value);
+  }
   return (
-    <span>Home</span>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((n)=>
+          <Nweet key={n.id} nweetObj={n} isOwner={n.creatorId === userObj.uid}/>
+        )}
+
+      </div>
+    </div>
   )
 };
 
